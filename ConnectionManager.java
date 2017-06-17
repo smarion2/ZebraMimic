dcc4f1d0522f87de3bd30745afe9736c9e54cc10
Sam Marion <smarion2@gmail.com>
2017-06-17 15:21:54 -0400

    Added basic ping pong tcp server

---
+++ ConnectionManager.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ConnectionManager {
+
+    private static final int timout = 3000;
+    private static final int Port = 6880;
+    private static final String IpAddress = "";
+    private static final boolean WhileConnected = true; 
+
+    public static void main (String args[]){
+        try {
+            ServerSocket input = new ServerSocket(IpAddress, Port);
+
+            while (WhileConnected){
+                try (
+                    Socket connectionSocket = input.accept();
+                    BufferedReader iptFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                    DataOutputStream optToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                ) {
+                    String TCPclientMess = iptFromClient.readLine();
+                    System.out.println("Sending message: " + TCPclientMess);
+                    optToClient.writeBytes(TCPclientMess);
+                }
+            }
+
+            
+        }
+    }
+}
