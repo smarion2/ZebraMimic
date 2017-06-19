730a4b6712cbf40eb0dc8bafe165ec7c92070137
Sam Marion <sam.marion@franwell.com>
2017-06-19 15:55:35 -0400

    better connection manager succesfully getting messages from PrintOps

--- ConnectionManager.java
+++ ConnectionManager.java
@@ -1,36 +1,48 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.InputStream;
 
 public class ConnectionManager {
 
     private static final int timout = 3000;
-    private static final int port = 6880;
+    private static final int port = 9100;
     private static final boolean whileConnected = true; 
+    private static final int bufferSize = 1024;
+    private static InputStream inputStream = null;
 
     public static void main (String args[]){
         try {
             ServerSocket input = new ServerSocket(port);
             System.out.println("Starting server...");
+
             while (whileConnected){
-                try (
+                try { 
                     Socket connectionSocket = input.accept();
-                    BufferedReader iptFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                    DataOutputStream optToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                ) {
-                    String tcpClientMess = iptFromClient.readLine();
-                    System.out.println("Sending message: " + tcpClientMess);
-                    optToClient.writeBytes(tcpClientMess);
+                    inputStream = new BufferedInputStream(connectionSocket.getInputStream());
+                    int read;
+                    if (connectionSocket.isConnected()){
+                        System.out.println("Socket is connected!");
+                    }
+                    byte[] buffer = new byte[bufferSize];
+                    while((read = inputStream.read(buffer)) != -1){
+                        String message = new String(buffer, "ASCII").trim();
+                        System.out.println(message);
+                    }
                 }
                 catch (Exception e) {
                     System.out.println("Error sending message: " + e);
+                }
+                finally {
+                    inputStream.close();
                 }
             }            
         }
         catch (Exception e){
             System.out.println("Error: " + e);
+        }
+        finally {
+            inputStream.close();
         }
     }
 }
