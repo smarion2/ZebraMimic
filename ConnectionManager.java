3fbcd5a01ea47824f5c0a10ed29672be64ef3a58
Sam Marion <sam.marion@franwell.com>
2017-06-19 10:42:16 -0400

    fixed casing to be more javaish

--- ConnectionManager.java
+++ ConnectionManager.java
@@ -7,22 +7,22 @@ import java.net.Socket;
 public class ConnectionManager {
 
     private static final int timout = 3000;
-    private static final int Port = 6880;
-    private static final boolean WhileConnected = true; 
+    private static final int port = 6880;
+    private static final boolean whileConnected = true; 
 
     public static void main (String args[]){
         try {
-            ServerSocket input = new ServerSocket(Port);
+            ServerSocket input = new ServerSocket(port);
             System.out.println("Starting server...");
-            while (WhileConnected){
+            while (whileConnected){
                 try (
                     Socket connectionSocket = input.accept();
                     BufferedReader iptFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
                     DataOutputStream optToClient = new DataOutputStream(connectionSocket.getOutputStream());
                 ) {
-                    String TCPclientMess = iptFromClient.readLine();
-                    System.out.println("Sending message: " + TCPclientMess);
-                    optToClient.writeBytes(TCPclientMess);
+                    String tcpClientMess = iptFromClient.readLine();
+                    System.out.println("Sending message: " + tcpClientMess);
+                    optToClient.writeBytes(tcpClientMess);
                 }
                 catch (Exception e) {
                     System.out.println("Error sending message: " + e);
