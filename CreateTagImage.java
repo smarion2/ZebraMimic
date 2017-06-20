01aea950bbec8ab7df1492082fecf8d10f2de9f9
Sam Marion <smarion2@gmail.com>
2017-06-20 00:21:09 -0400

    query labelary to make an image from the test zpl to simulate a print

---
+++ CreateTagImage.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
+import java.net.URL;
+import java.io.OutputStream;
+
+public class CreateTagImage {
+    public static void QueryAPI(String zpl) {
+        String type = "application/x-www-form-urlencoded";
+        String encodedData = URLEncoder.encode(zpl);
+        // dont forget to change dpmm i forget what the printer uses right now.
+        URL url = new URL("http://api.labelary.com/v1/printers/{dpmm}/labels/4x6/0/");
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", type);
+        conn.setRequestProperty("Content-length", String.valueOf(encodedData.length()));
+        OutputStream stream = conn.getOutputStream();
+        steam.write(encodedData.getBytes());
+
+        // convert bytes to image and save to folder.
+    }
+}
